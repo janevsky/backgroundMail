54ec1da95e680a6a3e90cd1bbcb88ca65a8e01a9
janevsky <63719545+janevsky@users.noreply.github.com>
2026-01-11 16:22:52 +0100

    Update copyright year and owner in index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  *
  * Wrapper for BackgroundMail plugin.
  *
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2026 Janevsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  */
 
